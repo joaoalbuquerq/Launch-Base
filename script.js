9a43dc39a37065e24c4538a1112e38d1358c78c8
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-04-14 22:10:41 -0300

    adicionando funcionalidade de inserir video dinamicamente via js

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const cards = document.querySelectorAll('.card')
 for(let card of cards){
     //Vai escutar o evento de click e quando tiver um click irá adicionar uma classe active
     card.addEventListener("click",function(){
-        modalOverlay.classList.add('active')
+
+        const videoid = card.getAttribute("id") //VAI PEGAR O ATRIBUTO ID DAS TAGS e atribuir a variavel videoid
+        modalOverlay.classList.add('active') 
+
+        /*irá selecionar o iframe dentro do documento html e a cada click irá inserir um video a id diferente de acordo com o card clicado */
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoid}`
+
+        
     })
 }
 
@@ -14,4 +21,8 @@ Irá selecionar todos os elementos que possuem a classe especificada
 irá escutar o evento de click, e quando acontecer irá remover da classe modal-overlay a classe active */
 document.querySelector(".close-modal").addEventListener("click",function(){
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src =""
 })
+
+//Procurar o iframe para pudermos trocar os videos
+
